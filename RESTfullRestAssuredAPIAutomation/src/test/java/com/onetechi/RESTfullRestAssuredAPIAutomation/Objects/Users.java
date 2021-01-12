6eb9a25788f59c4d608dc65606aa96a65cf6d801
Md Shahnewaz Zaman <md83shahnewaz@gmail.com>
2021-01-12 10:19:57 -0500

    Create Users.java

---
+++ RESTfullRestAssuredAPIAutomation/src/test/java/com/onetechi/RESTfullRestAssuredAPIAutomation/Objects/Users.java
@@ -0,0 +1,25 @@
+package Objects;
+
+public class Users {
+
+    public int page;
+    public int per_page;
+    public int total;
+    public int total_pages;
+
+    public int getPage() {
+        return page;
+    }
+
+    public int getPer_page() {
+        return per_page;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public int getTotal_pages() {
+        return total_pages;
+    }
+}
